2da8b1fa0a7a05672a21bc8742c7a0018b33d05b
hyun <duboo31@gmail.com>
2023-10-09 03:47:14 +0900

    폰트어썸 설치 및 스타일 코드 작성

--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -1,6 +1,17 @@
 import axios from "axios";
 import React, { useEffect, useRef, useState } from "react";
 import { Link, useNavigate } from "react-router-dom";
+// 디자인
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+  faEnvelope,
+  faLock,
+  faUser,
+  faM,
+  faBlog,
+  faArrowRight,
+} from "@fortawesome/free-solid-svg-icons";
+import "../styles/form.css";
 
 const Register = () => {
   console.log("Register 컴포넌트 마운트");
@@ -40,6 +51,9 @@ const Register = () => {
       return;
     } else if (!checkPWD) {
       setErrMsg("비밀번호는 영문, 숫자 조합 8자리 이상을 사용하세요.");
+      return;
+    } else if (password !== passwordCheck) {
+      setErrMsg("비밀번호가 일치하지 않습니다.");
       return;
     }
 
@@ -102,64 +116,100 @@ const Register = () => {
   }, [email, password, isPasswordConfirmed]);
 
   return (
-    <section>
-      <h1>회원가입 페이지</h1>
-      <div>이미 가입하셨나요?</div>
-      <Link to="/login">Login</Link>
-      <div>배너 이미지</div>
-      <form onSubmit={onSubmitSignupHandler}>
-        {/* <input
+    <section className="wrap">
+      <div className="form-container">
+        <div className="img-sample"></div>
+        <div className="form-content">
+          <h1>회원가입</h1>
+          <form className="form-items" onSubmit={onSubmitSignupHandler}>
+            {/* <input
           type="file"
           accept="image/*"
           onChange={(e) => setImgFile(e.target.files[0])}
           required
         /> */}
-        <input
-          type="text"
-          placeholder="Enter Email"
-          onChange={(e) => setEmail(e.target.value)}
-          value={email}
-          required
-          name="email"
-        />
-        <input
-          type="password"
-          placeholder="Enter Password"
-          onChange={(e) => setPassword(e.target.value)}
-          value={password}
-          required
-        />
-        <input
-          type="password"
-          placeholder="Repeat Password"
-          onChange={(e) => setPasswordCheck(e.target.value)}
-          value={passwordCheck}
-          required
-        />
-        <input
-          type="text"
-          placeholder="Nick Name"
-          onChange={(e) => setNickname(e.target.value)}
-          value={nickname}
-          required
-        />
-        <input
-          type="text"
-          placeholder="Mbti"
-          onChange={(e) => setMbti(e.target.value)}
-          value={mbti}
-        />
-        <input
-          type="text"
-          placeholder="Blog"
-          onChange={(e) => setBlog(e.target.value)}
-          value={blog}
-        />
-        <button disabled={!isPasswordConfirmed} type="submit">
-          회원가입
-        </button>
-      </form>
-      <p ref={errRef}>{errMsg}</p>
+            <div className="input-wrap">
+              <input
+                type="text"
+                onChange={(e) => setNickname(e.target.value)}
+                value={nickname}
+                required
+                placeholder="Username"
+              />
+              <FontAwesomeIcon className="input-icon" icon={faUser} />
+            </div>
+            <div className="input-wrap">
+              <input
+                type="text"
+                onChange={(e) => setEmail(e.target.value)}
+                value={email}
+                required
+                name="email"
+                placeholder="Email Address"
+              />
+              <FontAwesomeIcon className="input-icon" icon={faEnvelope} />
+            </div>
+            <div className="input-wrap">
+              <input
+                type="password"
+                onChange={(e) => setPassword(e.target.value)}
+                value={password}
+                required
+                placeholder="Password"
+              />
+              <FontAwesomeIcon className="input-icon" icon={faLock} />
+            </div>
+            <div className="input-wrap">
+              <input
+                type="password"
+                onChange={(e) => setPasswordCheck(e.target.value)}
+                value={passwordCheck}
+                required
+                placeholder="Confirm Password"
+              />
+              <FontAwesomeIcon className="input-icon" icon={faLock} />
+            </div>
+            <div className="input-wrap option-line">
+              <div className="option-text">Option</div>
+            </div>
+            <div className="input-wrap">
+              <input
+                type="text"
+                onChange={(e) => setBlog(e.target.value)}
+                value={blog}
+                placeholder="Blog Address"
+              />
+              <FontAwesomeIcon className="input-icon" icon={faBlog} />
+            </div>
+            <div className="input-wrap">
+              <input
+                type="text"
+                onChange={(e) => setMbti(e.target.value)}
+                value={mbti}
+                placeholder="MBTI"
+              />
+              <FontAwesomeIcon className="input-icon" icon={faM} />
+            </div>
+
+            <button className="submit-btn" type="submit">
+              가입하기
+              <FontAwesomeIcon className="btn-icon" icon={faArrowRight} />
+            </button>
+          </form>
+          <div className="form-footer">
+            <p className="err-text" ref={errRef}>
+              {errMsg}
+            </p>
+            <div className="other-line"></div>
+            <div className="other-wrap">
+              <span className="other-text">이미 가입하셨나요?</span>
+              <Link className="other-link" to="/login">
+                로그인
+              </Link>
+            </div>
+          </div>
+        </div>
+      </div>
     </section>
   );
 };
