303b5f733ffb34c1605651943a240ac7afc16e68
hyun <duboo31@gmail.com>
2023-10-11 10:04:39 +0900

    hook-form 제거

--- src/pages/Article.jsx
+++ src/pages/Article.jsx
@@ -1,5 +1,4 @@
 import React, { useEffect, useState } from "react";
-import { useForm } from "react-hook-form";
 
 // api
 import { createArticle } from "../js/api/createArticle";
@@ -7,70 +6,43 @@ import { useNavigate } from "react-router-dom";
 
 const Article = () => {
   console.log("Article 컴포넌트 마운트");
-  const {
-    register,
-    handleSubmit,
-    formState: { isSubmitting, errors },
-  } = useForm();
-
   const navigate = useNavigate();
 
-  // const [title, setTitle] = useState("");
-  // const [content, setContent] = useState("");
-  // const [tag, setTag] = useState("");
+  const [title, setTitle] = useState("");
+  const [content, setContent] = useState("");
+  const [tag, setTag] = useState("");
 
-  // const onClickSendArticleHandler = (e) => {
-  //   e.preventDefault();
-  //   createArticle(title, content, tag, navigate, setTitle, setContent);
-  // };
+  const onClickSendArticleHandler = (e) => {
+    e.preventDefault();
+    createArticle(title, content, tag, navigate, setTitle, setContent);
+  };
 
   return (
     <>
       <div>
-        <form
-          // onSubmit={onClickSendArticleHandler}
-          onSubmit={handleSubmit((data) => alert(JSON.stringify(data)))}
-        >
+        <form onSubmit={onClickSendArticleHandler}>
           <div>
             <input
               type="text"
               placeholder="제목을 입력하세요"
-              // onChange={(e) => setTitle(e.target.value)}
-              // value={title}
-              id="title"
-              {...register("title", {
-                required: "제목을 입력하세요!!",
-              })}
-              aria-invalid={errors.title ? "true" : "false"}
+              onChange={(e) => setTitle(e.target.value)}
+              value={title}
+              required
             />
-            {errors.title && <span>{errors.title.message}</span>}
             <input
               type="text"
               placeholder="태그/카테고리"
-              // onChange={(e) => setTag(e.target.value)}
-              // value={tag}
-              id="tag"
-              {...register("tag", {
-                required: "태그를 입력하세요!!",
-              })}
-              aria-invalid={errors.tag ? "true" : "false"}
+              onChange={(e) => setTag(e.target.value)}
+              value={tag}
             />
-            {errors.tag && <span>{errors.tag.message}</span>}
             <textarea
               type="text"
               placeholder="올해 목표를 위한 다짐을 적어보세요!"
-              // onChange={(e) => setContent(e.target.value)}
-              // value={content}
-              id="content"
-              {...register("content", {
-                required: "내용을 입력하세요!!",
-              })}
-              aria-invalid={errors.content ? "true" : "false"}
+              onChange={(e) => setContent(e.target.value)}
+              value={content}
+              required
             />
-            {errors.content && <span>{errors.content.message}</span>}
-            <button type="submit" disabled={isSubmitting}>
-              목표 등록하기
-            </button>
+            <button type="submit">목표 등록하기</button>
           </div>
         </form>
       </div>
